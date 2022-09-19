e87b869e2a692fa1c507b233b01a05ce80f37abc
jali <javed.ali@softwareag.com>
2022-09-19 15:19:42 +0530

    patch 2

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,46 +1,47 @@
-import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  //templateUrl: './product-list.component.html',
-  //templateUrl: './product-list.table-component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products:Product[];
-  currentCategoryId: number;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
 
   constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
-  ngOnInit(): void {
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
+  }
+
+  listProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     }
-  listProducts(){
-   // check if "id" parameter is available
-   const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
 
-   if (hasCategoryId) {
-     // get the "id" param string. convert string to a number using the "+" symbol
-     this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-   }
-   else {
-     // not category id available ... default to category id 1
-     this.currentCategoryId = 1;
-   }
-
-   // now get the products for the given category id
-   this.productService.getProductList(this.currentCategoryId).subscribe(
-     data => {
-       this.products = data;
-     }
-   )
- }
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,31 +1,30 @@
+import { Product } from './../common/product';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '../common/product';
-import { map } from 'rxjs/operators'
-
-
+import {map} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private baseUrl = "http://localhost:8080/api/products"
+
+  private baseUrl = 'http://localhost:8081/api/products';
+
 
   constructor(private httpClient:HttpClient) { }
 
-  getProductList(theCategoryId: number):Observable<Product[]>{
+  getProductList(theCategoryId:number): Observable<Product[]>{
 
-     // need to build URL based on category id 
-     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.httpClient.get<GetResponse>(searchUrl).pipe(
-      map(response=>response._embedded.products)
-    );
+      map(response=>response._embedded.products));
   }
 }
-  interface GetResponse {
-    _embedded: {
-      products: Product[];
-    }
+
+interface GetResponse{
+  _embedded:{
+    products:Product[]
+  }
 }
